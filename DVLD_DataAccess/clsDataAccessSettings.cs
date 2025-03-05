895cd7845937aa0bcd1dca474b1b4815e3a40806
Abdallh Alhelel <abdallh20hl@gmail.com>
2025-03-05 18:59:08 +0300

    Add DVLD Files

---
+++ DVLD_DataAccess/clsDataAccessSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DVLD_DataAccess
+{
+    static class clsDataAccessSettings
+    {
+        public static string ConnectionString = @"server=.\MSSQLSERVER22;User id=abdullah;database=DVLD;Integrated Security=True;";
+
+
+    }
+}
