72c2029ac06496d91b1c34b8b1c3c5060a93e410
Abdallh Alhelel <abdallh20hl@gmail.com>
2025-03-15 22:12:18 +0300

    fix confilect

--- DVLD_Buisness/clsCountry.cs
+++ DVLD_Buisness/clsCountry.cs
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 ﻿using System;
 using System.Data;
 using DVLD_DataAccess;
@@ -59,65 +59,3 @@ namespace DVLD_Buisness
 
     }
 }
-=======
-﻿using System;
-using System.Data;
-using DVLD_DataAccess;
-
-namespace DVLD_Buisness
-{
-    public class clsCountry
-    {
-
-        public int ID { set; get; }
-        public string CountryName { set; get; }
-   
-        public clsCountry()
-
-        {
-            this.ID = -1;
-            this.CountryName = "";
-
-        }
-
-        private clsCountry(int ID, string CountryName)
-
-        {
-            this.ID = ID;
-            this.CountryName = CountryName;
-        }
-
-        public static clsCountry Find(int ID)
-        {
-            string CountryName = "";
-
-            if (clsCountryData.GetCountryInfoByID(ID, ref CountryName))
-
-                return new clsCountry(ID, CountryName);
-            else
-                return null;
-
-        }
-
-        public static clsCountry Find(string CountryName)
-        {
-
-            int ID = -1;
-           
-            if (clsCountryData.GetCountryInfoByName(CountryName, ref ID ))
-
-                return new clsCountry(ID, CountryName);
-            else
-                return null;
-
-        }
-
-        public static DataTable GetAllCountries()
-        {
-            return clsCountryData.GetAllCountries();
-
-        }
-
-    }
-}
->>>>>>> a586df740e777fdc9413b893c9ae28405c71acc6
